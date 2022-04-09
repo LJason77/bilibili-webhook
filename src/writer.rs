3caea71f0edeb1af09562f75691b3fc49a1f6a4e
LJason <ljason@ljason.cn>
2022-04-10 07:55:38 +0800

    添加创建文件的选项
    Signed-off-by: LJason <ljason@ljason.cn>

--- src/writer.rs
+++ src/writer.rs
@@ -10,17 +10,18 @@ pub fn bilili(source: &str, output: &str) {
     create_dir_all("config/bilili").unwrap_or_else(|error| {
         error!("{:?}", error);
     });
-    let mut file = OpenOptions::new().append(true).open(&log_file).unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create(&log_file).unwrap_or_else(|error| {
+    let mut file =
+        OpenOptions::new().create(true).append(true).open(&log_file).unwrap_or_else(|error| {
+            if error.kind() == ErrorKind::NotFound {
+                File::create(&log_file).unwrap_or_else(|error| {
+                    error!("{:?}", error);
+                    panic!();
+                })
+            } else {
                 error!("{:?}", error);
                 panic!();
-            })
-        } else {
-            error!("{:?}", error);
-            panic!();
-        }
-    });
+            }
+        });
     file.write_all(output.as_bytes()).expect("写入失败");
     file.write_all("\n".as_bytes()).expect("写入失败");
 }
